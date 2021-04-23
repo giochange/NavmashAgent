d5ac82efda7a126afa52e1e3c8d2f29732990e69
giovanni de camargo <giosilg@gmail.com>
2021-04-23 19:09:14 -0300

    upgrade
    upload

---
+++ Assets/Scripts/AIControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // setagem  do componente navmash dentro da unity 
+     void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+}
---
+++ Assets/Scripts/AgentManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+
+    GameObject[] agents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // pegada da posição do objeto com tag "ai" fazendo assim fução de busca
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //condicionamento de ação perante ao click do mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            //criação do raycast para identificação  do solo setando assim em comparação com a posição que foi clicada pelo mouse.
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                //resultado do condicionamento que resulta no endereçamento dos pontos clicados e ao mesmo tempo guardando a posição
+                foreach (GameObject a in agents)
+            a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+
+
+            }
+
+        }
+
+    }
+}
